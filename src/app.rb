f9452562344afdcc5f593c174c93d16955554670
Felix C. Stegerman <flx@obfusk.net>
2012-08-18 23:32:08 +0200

    [src/app.rb] header, folds
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- src/app.rb
+++ src/app.rb
@@ -1,6 +1,15 @@
-require 'haml'
+# --                                                            # {{{1
+#
+# File        : src/app.rb
+# Maintainer  : Felix C. Stegerman <flx@obfusk.net>
+# Date        : 2012-08-18
+#
+# Copyright   : Copyright (C) 2012  Felix C. Stegerman
+# Licence     : GPLv2
+#
+# --                                                            # }}}1
 
-# --
+require 'haml'
 
 require './cfg'
 
@@ -18,33 +27,33 @@ end                                                             # }}}1
 
 # --
 
-helpers do                                                      # {{{1
+helpers do
   def r (what, *more)
     to(R[what][*more])
   end
 
-  def icon (s)
+  def icon (s)                                                  # {{{1
     case s
       when 'dead'   ; 'warning-sign'
       when 'stopped'; 'off'
       when 'running'; 'ok'
       else raise '...'                                          # TODO
     end
-  end
+  end                                                           # }}}1
 
-  def naps
+  def naps                                                      # {{{1
     # TODO: check return value etc.
 
     %x[ naps list ].split.map do |x|
       s, t  = %x[ nap status #{x} -s ].strip.split ' ', 2
       { name: x, stat: s, time: t, icon: icon(s) }
     end
-  end
+  end                                                           # }}}1
 
   def nap_info (x)
     # TODO
   end
-end                                                             # }}}1
+end
 
 # --
 
@@ -53,29 +62,31 @@ R = {
   app:  ->(x) { "/app/#{x}" },
 }
 
-# -- [routes] --                                                # {{{1
+# --
 
-before do
+before do                                                       # {{{1
   @layout_css = []
   @layout_js  = []
 
   @title = @naps = @info = nil
-end
+end                                                             # }}}1
 
 get '/' do
   redirect r(:naps)
 end
 
-get '/naps' do
+get '/naps' do                                                  # {{{1
   @title  = 'naps'
   @naps   = naps
   haml :naps
-end
+end                                                             # }}}1
 
-get %r[/app/([a-z_-]+)] do |app|
+get %r[^/app/([a-z_-]+)$] do |app|                              # {{{1
   @title  = "nap info #{app}"
   @info   = nap_info app
   haml :app
-end
+end                                                             # }}}1
 
-# --                                                            # }}}1
+# --
+
+# vim: set tw=70 sw=2 sts=2 et fdm=marker :
